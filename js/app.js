ed42e0b22835ecb009d138a18e10974dab2d11d8
Python-Maniac <Pytonmaniacxxl@gmail.com>
2020-05-05 16:31:42 +0300

    JS modals & buttoms

--- js/app.js
+++ js/app.js
@@ -1,31 +1,86 @@
-$(function(){
+$(function () {
 
+
+    // FILTER
     let filter = $("[data-filter]");
 
-    filter.on("click", function(event){
+    filter.on("click", function (event) {
         event.preventDefault();
 
         let cat = $(this).data("filter");
 
-        if(cat == "all") {
+        if (cat == "all") {
             $("[data-cat]").removeClass("hide");
         } else {
-            $("[data-cat]").each(function(){
+            $("[data-cat]").each(function () {
 
                 let workCat = $(this).data("cat");
-    
-                if(workCat != cat) {
+
+                if (workCat != cat) {
                     $(this).addClass("hide");
                 } else {
                     $(this).removeClass("hide");
                 }
-    
             });
         }
-
-        
     });
 
+
+    // MODAL
+    const modalCall = $("[data-modal]");
+    const modalClose = $("[data-close]");
+
+    modalCall.on("click", function (event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalId = $this.data("modal");
+
+        $(modalId).addClass('show');
+        $("body").addClass("no-scroll");
+
+        setTimeout(function () {
+            $(modalId).find(".modal__dialog").css({
+                transform: "rotateX(0)"
+            });
+        }, 200);
+
+    });
+
+    modalClose.on("click", function (event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalParent = $this.parents(".modal");
+
+        modalParent.find(".modal__dialog").css({
+            transform: "rotateX(90deg)"
+        });
+
+        setTimeout(function () {
+            modalParent.removeClass('show');
+            $("body").removeClass("no-scroll");
+        }, 200);
+
+    });
+
+    $(".modal").on("click", function (event) {
+        let $this = $(this);
+
+        $this.find(".modal__dialog").css({
+            transform: "rotateX(90deg)"
+        });
+
+        setTimeout(function () {
+            $this.removeClass('show');
+            $("body").removeClass("no-scroll");
+        }, 200);
+
+    });
+
+    $(".modal__dialog").on("click", function (event) {
+        event.stopPropagation();
+    });
 
 
 });
